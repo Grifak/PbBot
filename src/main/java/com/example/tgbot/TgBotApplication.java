3a463a59ccd870a383cb1d1a24ca11be2e682477
vadim.nepomnyashchii <vadim.nepomnyashchii@simbirsoft.com>
2022-08-29 00:03:03 +0300

    add pingService

--- src/main/java/com/example/tgbot/TgBotApplication.java
+++ src/main/java/com/example/tgbot/TgBotApplication.java
@@ -4,9 +4,11 @@ import com.example.tgbot.config.BotConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableConfigurationProperties(BotConfig.class)
+@EnableConfigurationProperties({BotConfig.class})
+@EnableScheduling
 public class TgBotApplication {
 
     public static void main(String[] args) {
